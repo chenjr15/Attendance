5ec49b5d347b8cb64924c17c1783e4adb3bccbe3
chenjr15 <chenjr15@live.cn>
2021-03-14 23:23:10 +0800

    新建工程

---
+++ src/main/java/dev/chenjr/attendance/AttendanceApplication.java
@@ -0,0 +1,22 @@
+package dev.chenjr.attendance;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+public static void main(String[] args) {
+	SpringApplication.run(DemoApplication.class, args);
+}
+
+@GetMapping("/")
+	public String index(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}

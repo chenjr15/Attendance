87295f8b69b0080191121f96a83a771980f54838
chenjr15 <chenjr15@live.cn>
2021-04-06 10:29:42 +0800

    创建短信服务接口

---
+++ src/main/java/dev/chenjr/attendance/service/IShortMessageService.java
@@ -0,0 +1,23 @@
+package dev.chenjr.attendance.service;
+
+public interface IShortMessageService extends IService {
+
+    /**
+     * 发送验证码 指定手机号和类型
+     *
+     * @param phone 手机号
+     * @param type  类型
+     * @return 发送结果
+     */
+    boolean sendCode(String phone, String type);
+
+    /**
+     * 判断验证码是否有效，匹配、过期
+     *
+     * @param phone 手机号
+     * @param type  验证码类型
+     * @param code  要匹配的验证码
+     * @return 是否有效
+     */
+    boolean codeValid(String phone, String type, String code);
+}

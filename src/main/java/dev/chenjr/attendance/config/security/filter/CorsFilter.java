6d28a9ab4fcce003becd0c2c78b87e57ed5d5eec
chenjr15 <chenjr15@live.cn>
2021-04-11 23:57:36 +0800

    去除输出

--- src/main/java/dev/chenjr/attendance/config/security/filter/CorsFilter.java
+++ src/main/java/dev/chenjr/attendance/config/security/filter/CorsFilter.java
@@ -12,7 +12,6 @@ public class CorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws ServletException, IOException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        System.out.println("CORS FILETER");
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Headers", "*");
         filterChain.doFilter(servletRequest, servletResponse);

7a642150623807821ce4ac5318a430b37b181a7f
chenjr15 <chenjr15@live.cn>
2021-04-18 16:13:15 +0800

    ✨ 引入redis

--- src/main/java/dev/chenjr/attendance/service/ICacheService.java
+++ src/main/java/dev/chenjr/attendance/service/ICacheService.java
@@ -4,4 +4,16 @@ package dev.chenjr.attendance.service;
  * 缓存支持服务，如Redis
  */
 public interface ICacheService extends IService {
+
+    void setValue(String key, String value, long expireSec);
+
+    void setValue(String key, String value);
+
+    String getValue(String key);
+
+    void setHashValue(String setName, String key, String value);
+
+    void setHashValue(String setName, String key, String value, long expireSec);
+
+    String getHashValue(String setName, String key);
 }

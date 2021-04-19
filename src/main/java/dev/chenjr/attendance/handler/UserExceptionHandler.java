400c6ece044caa2159042cd33091b2aeb2651bb9
chenjr15 <chenjr15@live.cn>
2021-04-20 00:02:13 +0800

    🚑 用户名未找到异常处理

--- src/main/java/dev/chenjr/attendance/handler/UserExceptionHandler.java
+++ src/main/java/dev/chenjr/attendance/handler/UserExceptionHandler.java
@@ -5,6 +5,7 @@ import dev.chenjr.attendance.service.dto.RestResponse;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,11 +13,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class UserExceptionHandler {
-    
+
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(RegisterException.class)
     public RestResponse<String> handleRegisterExceptions(RegisterException ex) {
+        return new RestResponse<>(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), null);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public RestResponse<String> handleUsernameNotFoundExceptions(UsernameNotFoundException ex) {
         return new RestResponse<>(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), null);
     }
 

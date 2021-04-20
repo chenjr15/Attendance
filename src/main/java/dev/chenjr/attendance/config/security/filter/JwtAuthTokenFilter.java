ee184bc914e99ffd00f2d50f6927e33f3548c4d6
chenjr15 <chenjr15@live.cn>
2021-04-20 10:41:48 +0800

    ✏ sys.out.println->日志输出

--- src/main/java/dev/chenjr/attendance/config/security/filter/JwtAuthTokenFilter.java
+++ src/main/java/dev/chenjr/attendance/config/security/filter/JwtAuthTokenFilter.java
@@ -1,6 +1,7 @@
 package dev.chenjr.attendance.config.security.filter;
 
 import dev.chenjr.attendance.utils.JwtTokenUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,7 +17,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
+@Slf4j
 @Component
 public class JwtAuthTokenFilter extends OncePerRequestFilter {
     @Autowired
@@ -33,7 +34,7 @@ public class JwtAuthTokenFilter extends OncePerRequestFilter {
         if (token != null && token.length() > 7) {
             token = token.substring(7);
             String username = jwtTokenUtil.getUsernameFromToken(token);
-            System.out.println(username);
+            log.info("JwtAuthTokenFilter get username :+" + username);
             if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
                 try {
 

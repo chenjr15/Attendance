444aec7bc47110e65fe76f332b7b80622b5b00c5
chenjr15 <chenjr15@live.cn>
2021-04-23 15:51:39 +0800

    📝 添加校验和值限定

--- src/main/java/dev/chenjr/attendance/service/dto/SmsCodeDTO.java
+++ src/main/java/dev/chenjr/attendance/service/dto/SmsCodeDTO.java
@@ -4,10 +4,12 @@ import dev.chenjr.attendance.service.dto.validation.PhoneNumber;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 public class SmsCodeDTO {
     @NotBlank
-    @Schema(example = "register,login,reset_password")
+    @Schema(example = "register", allowableValues = {"register", "login", "reset_password"})
+    @Pattern(regexp = "(register)|(login)|(reset_password)", message = "仅接受register/login/reset_password")
     public String type;
     @NotBlank
     @PhoneNumber

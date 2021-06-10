10f05b2a0310392abaeb6aec2844f97875a943e7
chenjr15 <chenjr15@live.cn>
2021-06-10 22:09:55 +0800

    ✨ 加入校验

--- src/main/java/dev/chenjr/attendance/service/dto/OrganizationDTO.java
+++ src/main/java/dev/chenjr/attendance/service/dto/OrganizationDTO.java
@@ -1,24 +1,49 @@
 package dev.chenjr.attendance.service.dto;
 
+import dev.chenjr.attendance.service.dto.group.CreateGroup;
+import dev.chenjr.attendance.service.dto.group.ModifyGroup;
+import dev.chenjr.attendance.service.dto.validation.KeyWord;
+import dev.chenjr.attendance.service.dto.validation.KeyWordValidator;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @Data
 public class OrganizationDTO {
+  
+  @NotBlank(groups = ModifyGroup.class)
   @Schema(description = "组织结构id")
   private Long id;
+
+  @NotBlank(groups = CreateGroup.class)
   @Schema(description = "父级id，如果为0则为顶级结构", example = "0")
   private Long parentId;
+
+  @NotBlank(groups = CreateGroup.class)
   @Schema(description = "组织结构名称", example = "某某大学")
   private String name;
+
+  @Length(max = 2048)
   @Schema(description = "关于该结构的附加说明")
   private String comment;
-  @Schema(description = "类型", example = "院校")
+
+  @KeyWord
+  @NotBlank(groups = CreateGroup.class)
+  @Schema(description = "类型", example = "院校", pattern = KeyWordValidator.RE_KEYWORD)
   private String orgType;
-  @Schema(description = "所属省份，，0为其他", example = "北京")
+
+  @Schema(description = "所属省份，", example = "北京")
   private String province;
+
+  @Schema(description = "所属省份id", example = "11")
+  private String province_id;
+
+  @Schema(description = "所有的祖先节点", example = "xx大学-xx学院-xx专业")
+  private String parents;
+
   @Schema(description = "子结构数量")
   private Long childrenCount;
   @Schema(description = "子结构")

9ba3d15b58645c3a90005e498453b92358d8a8dc
chenjr15 <chenjr15@live.cn>
2021-06-27 21:23:35 +0800

    🐛 数据校验

--- src/main/java/dev/chenjr/attendance/service/dto/RoleDTO.java
+++ src/main/java/dev/chenjr/attendance/service/dto/RoleDTO.java
@@ -6,6 +6,9 @@ import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class RoleDTO {
     @JsonSerialize(using = ToStringSerializer.class)
@@ -13,8 +16,10 @@ public class RoleDTO {
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private Long id;
     
+    @NotNull
     @Schema(description = "中文名")
     private String name;
+    @NotEmpty
     @Schema(description = "英文名")
     private String code;
     @Schema(description = "顺序")

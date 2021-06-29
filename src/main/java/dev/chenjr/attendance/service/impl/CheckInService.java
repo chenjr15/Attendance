0cd57a0063867af65c03b73d1af11a8891e44fad
chenjr15 <chenjr15@live.cn>
2021-06-30 00:23:56 +0800

    🔊

--- src/main/java/dev/chenjr/attendance/service/impl/CheckInService.java
+++ src/main/java/dev/chenjr/attendance/service/impl/CheckInService.java
@@ -14,6 +14,7 @@ import dev.chenjr.attendance.service.ICheckInService;
 import dev.chenjr.attendance.service.ISysParamService;
 import dev.chenjr.attendance.service.IUserService;
 import dev.chenjr.attendance.service.dto.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,6 +26,7 @@ import java.util.stream.Collectors;
 import static dev.chenjr.attendance.dao.entity.CheckInLog.*;
 import static java.lang.Math.toRadians;
 
+@Slf4j
 @Service
 public class CheckInService implements ICheckInService {
     @Autowired
@@ -37,10 +39,10 @@ public class CheckInService implements ICheckInService {
     CourseMapper courseMapper;
     @Autowired
     UserCourseMapper userCourseMapper;
-
+    
     @Autowired
     ISysParamService sysParamService;
-
+    
     /**
      * @param pageSort 分页排序筛选
      * @return 所有的签到任务(分页后)
@@ -54,8 +56,8 @@ public class CheckInService implements ICheckInService {
         List<CheckInTaskDTO> collected = page.getRecords().stream().map(this::task2dto).collect(Collectors.toList());
         return PageWrapper.fromList(page, collected);
     }
-
-
+    
+    
     /**
      * 返回某个课程的签到任务
      *
@@ -65,7 +67,7 @@ public class CheckInService implements ICheckInService {
      */
     @Override
     public PageWrapper<CheckInTaskDTO> listCourseTasks(long courseId, PageSort pageSort) {
-
+        
         QueryWrapper<CheckInTask> qw = new QueryWrapper<>();
         qw = qw.eq("course_id", courseId);
         Page<CheckInTask> page = pageSort.getPage();
@@ -74,7 +76,7 @@ public class CheckInService implements ICheckInService {
         List<CheckInTaskDTO> collected = page.getRecords().stream().map(this::task2dto).collect(Collectors.toList());
         return PageWrapper.fromList(page, collected);
     }
-
+    
     /**
      * 获取某次签到的详细信息
      *
@@ -89,7 +91,7 @@ public class CheckInService implements ICheckInService {
         }
         return task2dto(checkInTask);
     }
-
+    
     /**
      * 修改签到任务
      *
@@ -103,13 +105,13 @@ public class CheckInService implements ICheckInService {
         if (!exists.orElse(false)) {
             throw HttpStatusException.notFound();
         }
-
+        
         CheckInTask checkInTask = dto2task(checkInTaskDTO);
         checkInTask.updateBy(updater.getId());
         taskMapper.updateById(checkInTask);
         return this.getTask(checkInTask.getId());
     }
-
+    
     /**
      * 获取签到记录
      *
@@ -127,8 +129,8 @@ public class CheckInService implements ICheckInService {
         List<CheckInResultDTO> collect = page.getRecords().stream().map(this::log2result).collect(Collectors.toList());
         return PageWrapper.fromList(page, collect);
     }
-
-
+    
+    
     /**
      * 删除签到任务
      *
@@ -141,7 +143,7 @@ public class CheckInService implements ICheckInService {
         }
         taskMapper.deleteById(taskId);
     }
-
+    
     /**
      * 修改签到任务
      *
@@ -158,14 +160,14 @@ public class CheckInService implements ICheckInService {
         logMapper.updateById(checkInLog);
         return this.getLog(checkInLog.getId());
     }
-
+    
     /**
      * 获取某次签到记录的详细信息
      *
      * @param logId 记录id
      * @return 详细信息
      */
-
+    
     public CheckInLogDTO getLog(long logId) {
         CheckInLog checkInLog = logMapper.selectById(logId);
         if (checkInLog == null) {
@@ -173,7 +175,7 @@ public class CheckInService implements ICheckInService {
         }
         return log2dto(checkInLog);
     }
-
+    
     /**
      * 签到
      *
@@ -205,7 +207,7 @@ public class CheckInService implements ICheckInService {
         }
         logDTO.setDistance(-1.0);
         logDTO.setCourseId(task.getCourseId());
-
+        
         /* 5. 计算经验值 */
         switch (logDTO.getStatus()) {
             case STATUS_NORMAL:
@@ -216,13 +218,13 @@ public class CheckInService implements ICheckInService {
                             logDTO.getLatitude(), logDTO.getLongitude(),
                             task.getLatitude(), task.getLongitude());
                     logDTO.setDistance(distance);
-
+                    
                     if (distance > 1000) {
                         throw HttpStatusException.badRequest("签到失败：你太远了！" + distance + "m");
                     }
                     logDTO.setDistance(distance);
                 }
-
+                
                 logDTO.setExperience(2);
                 break;
             case STATUS_LEAVE:
@@ -232,15 +234,15 @@ public class CheckInService implements ICheckInService {
             default:
                 logDTO.setExperience(0);
         }
-
+        
         CheckInLog checkInLog = dto2log(logDTO);
         checkInLog.createBy(operator.getId());
         logMapper.insert(checkInLog);
         logDTO.setId(checkInLog.getId());
-
+        
         return logDTO;
     }
-
+    
     /**
      * 创建签到任务/发起签到
      *
@@ -249,6 +251,10 @@ public class CheckInService implements ICheckInService {
      */
     @Override
     public CheckInTaskDTO createCheckInTask(CheckInTaskDTO checkInTaskDTO) {
+        
+        
+        log.info("创建签到任务:{}", checkInTaskDTO);
+        
         Long courseId = checkInTaskDTO.getCourseId();
         Optional<Boolean> exists = courseMapper.exists(courseId);
         if (!exists.orElse(false)) {
@@ -263,7 +269,7 @@ public class CheckInService implements ICheckInService {
         taskMapper.insert(checkInTask);
         return getTask(checkInTask.getId());
     }
-
+    
     /**
      * 结束签到
      *
@@ -287,7 +293,7 @@ public class CheckInService implements ICheckInService {
             taskMapper.updateById(task);
         }
     }
-
+    
     /**
      * 修改某个任务下某个学生的签到情况
      *
@@ -328,7 +334,7 @@ public class CheckInService implements ICheckInService {
         }
         return getLog(checkInLog.getId());
     }
-
+    
     /**
      * 获取某个课程当前的签到任务，如果没有签到任务会返回404
      *
@@ -337,15 +343,15 @@ public class CheckInService implements ICheckInService {
      */
     @Override
     public CheckInTaskDTO getCurrentCheckInTask(long courseId) {
-
+        
         CheckInTask task = taskMapper.current(courseId, LocalDateTime.now());
         if (task == null) {
             throw HttpStatusException.notFound("当前未发起签到或已经截止！");
         }
-
+        
         return task2dto(task);
     }
-
+    
     /**
      * 获取未签到的学生信息
      *
@@ -358,16 +364,16 @@ public class CheckInService implements ICheckInService {
         List<Long> checkedStu = logMapper.listChecked(taskId);
         List<Long> allStu = userCourseMapper.listElected(taskId);
         // 2. 计算未签到成员
-
-
+        
+        
         List<CheckInResultDTO> uncheckStuDTO = allStu.stream()
                 .filter(id -> !checkedStu.contains(id))
                 .map(this::getUncheckResult)
                 .collect(Collectors.toList());
-
+        
         return PageWrapper.singlePage(uncheckStuDTO);
     }
-
+    
     private CheckInResultDTO getUncheckResult(Long uid) {
         CheckInResultDTO resultDTO = new CheckInResultDTO();
         resultDTO.setUid(uid);
@@ -379,9 +385,9 @@ public class CheckInService implements ICheckInService {
         resultDTO.setDistance(-1.0);
         return resultDTO;
     }
-
+    
     private String getStatusName(int status) {
-
+        
         switch (status) {
             case STATUS_NORMAL:
                 return "已签到";
@@ -394,7 +400,7 @@ public class CheckInService implements ICheckInService {
         }
         return "未知";
     }
-
+    
     private int getExperience(int status) {
         SysParameterDTO expDto = sysParamService.getSystemParam("sys_check_in_exp");
         int exp = Integer.parseInt(expDto.getValue());
@@ -410,7 +416,7 @@ public class CheckInService implements ICheckInService {
         }
         return exp;
     }
-
+    
     private CheckInLog dto2log(CheckInLogDTO logDTO) {
         CheckInLog checkInLog = new CheckInLog();
         checkInLog.setLatitude(logDTO.getLatitude());
@@ -421,10 +427,10 @@ public class CheckInService implements ICheckInService {
         checkInLog.setId(logDTO.getId());
         checkInLog.setExperience(logDTO.getExperience());
         checkInLog.setCourseId(logDTO.getCourseId());
-
+        
         return checkInLog;
     }
-
+    
     private CheckInLogDTO log2dto(CheckInLog checkInLog) {
         CheckInLogDTO logDTO = new CheckInLogDTO();
         logDTO.setId(checkInLog.getId());
@@ -440,7 +446,7 @@ public class CheckInService implements ICheckInService {
         logDTO.setStuName(userById.getRealName());
         return logDTO;
     }
-
+    
     private CheckInResultDTO log2result(CheckInLog checkInLog) {
         CheckInResultDTO resultDTO = new CheckInResultDTO();
         resultDTO.setUid(checkInLog.getUid());
@@ -458,10 +464,10 @@ public class CheckInService implements ICheckInService {
         User userById = userService.getUserById(uid);
         resultDTO.setStuId(userById.getAcademicId());
         resultDTO.setStuName(userById.getRealName());
-
+        
         return resultDTO;
     }
-
+    
     private CheckInTaskDTO task2dto(CheckInTask record) {
         CheckInTaskDTO checkInTaskDTO;
         checkInTaskDTO = new CheckInTaskDTO();
@@ -480,25 +486,31 @@ public class CheckInService implements ICheckInService {
         checkInTaskDTO.setOperatorName(userService.getRealNameById(operator));
         return checkInTaskDTO;
     }
-
+    
     private CheckInTask dto2task(CheckInTaskDTO dto) {
         CheckInTask entity;
         entity = new CheckInTask();
         entity.setId(dto.getId());
         entity.setType(dto.getType());
-
+        
         entity.setDeadline(dto.getDeadline());
-
+        
         entity.setCourseId(dto.getCourseId());
         entity.setDescription(dto.getDescription());
-
+        
+        if (dto.getLongitude() == null) {
+            dto.setLongitude(0.0);
+        }
+        if (dto.getLatitude() == null) {
+            dto.setLatitude(0.0);
+        }
         entity.setLongitude(dto.getLongitude());
         entity.setLatitude(dto.getLatitude());
         entity.setParam(dto.getParam());
         entity.setUpdater(dto.getOperatorId());
         return entity;
     }
-
+    
     /**
      * 距离计算
      *

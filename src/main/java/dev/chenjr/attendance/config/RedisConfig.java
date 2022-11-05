d1ffc0704ec260fe468705334a19f0fbf1c4f6b2
chenjr15 <chenjr15@live.cn>
2022-11-05 22:11:32 +0800

    🔧 增加更多过期时间的修改

--- src/main/java/dev/chenjr/attendance/config/RedisConfig.java
+++ src/main/java/dev/chenjr/attendance/config/RedisConfig.java
@@ -1,29 +1,28 @@
 package dev.chenjr.attendance.config;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.cache.CacheManager;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
 
+@Slf4j
 @Configuration
 public class RedisConfig {
+    @Value("${spring.cache.redis.time-to-live:1h}")
+    Duration ttl;
+    
     @Bean
     public RedisCacheConfiguration cacheConfiguration() {
+        log.info("TTL:" + ttl);
         return RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofMinutes(60))
+                .entryTtl(ttl)
                 .disableCachingNullValues()
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
     }
+    
 }
